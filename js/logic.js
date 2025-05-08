f12a8a74364162598c5cf7202dbe5e791c61a019
Erin-mp4 <erinobrien522@gmail.com>
2025-05-08 15:21:01 -0500

    Logic and Modal Styling
    Wrote up the logic to open and close modal windows. Also styled the modal windows and created keyframes for a smoother UX.

--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,18 @@
-$(".button").on("click", function() {
-    var modal = $(this).data("modal");
-    $(modal).show();
-});
-$(".modal").on("click", function(e) {
-    var className = e.target.className;
-    if(className === "modal" || className === "close"){
-        $(this).closest(".modal").hide();
-    }
-});
+function olderPick() {
+    document.getElementById('older').style.display = "flex";
+}
+function oldPick() {
+    document.getElementById('old').style.display = "flex";
+}
+function newPick() {
+    document.getElementById('new').style.display = "flex";
+}
+function newerPick() {
+    document.getElementById('newer').style.display = "flex";
+}
+function closePick() {
+    document.getElementById('older').style.display = "none";
+    document.getElementById('old').style.display = "none";
+    document.getElementById('new').style.display = "none";
+    document.getElementById('newer').style.display = "none";
+}
